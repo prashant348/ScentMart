c915cdff9ffe78158e72fe7cef7db9afb4318bd5
codingweapon <prashantmistri348@gmail.com>
2025-12-31 16:43:32 +0530

    feat: implement order placing and quantity selection logic
    This commit sets up the core logic for:
    
    - order placing
    - select quantity
    
    data operation of order placing is handled via server actions

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -2,6 +2,7 @@
 
 import { supabase } from "@/libs/supabase";
 import { auth } from "@clerk/nextjs/server";
+import { use } from "react";
 
 export async function addProduct(formData: FormData) {
     
@@ -36,5 +37,44 @@ export async function fetchProduts() {
         return data
     } catch (e) {
         console.error("error in fetching products: ", e)
+    }
+}
+
+export async function createOrder(productId: string, price: number, quantity: number) {
+    const { userId } = await auth();
+    if (!userId) return;
+
+    try {
+        const { data: order, error: orderError } = await supabase
+            .from("orders")
+            .insert({
+                user_id: userId,
+                total_amount: price,
+            })
+            .select()
+            .single();
+        
+        if (orderError || !order) {
+            console.log("orders insert failed!: ", orderError);
+            return;
+        }
+
+        const { error: orderItemError } = await supabase
+            .from("order_items")
+            .insert({
+                order_id: order?.id,
+                product_id: productId,
+                quantity: quantity,
+                price,
+            });
+        
+        if (orderItemError) {
+            console.log("order items insert failed!: ", orderItemError);
+            return;
+        }
+        
+        console.log("order created successfully!");
+    } catch (e) {
+        console.error("error in creating order: ", e)
     }
 }
--- components/DashboardContent.tsx
+++ components/DashboardContent.tsx
@@ -3,7 +3,9 @@
 // import { useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { addProduct } from "@/app/dashboard/actions";
+import { createOrder } from "@/app/dashboard/actions";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface Product {
     id: string
@@ -14,11 +16,20 @@ interface Product {
 export default function DashboardContent({ products }: { products: Product[] }) {
 
     // const [ isRefreshing, setIsRefreshing ] = useState<boolean>(false);
+    // A Record type is a built-in utility that constructs a new object type with a specified key type and value type
+    const [ counts, setCounts ] = useState<Record<string, number>>({ });
     const { user } = useUser();
-    if (!user) return;
+    if (!user) return null;
     const fullName = `${user.firstName} ${user.lastName}`;
     const router = useRouter();
 
+    const increment  = (id: string) => {
+        setCounts(prev => ({...prev, [id]: (prev[id] || 0) + 1}));
+    }
+
+    const decrement = (id: string) => {
+        setCounts(prev => ({...prev, [id]: Math.max(0, (prev[id] ?? 0) - 1)}));
+    }
     return (
         <div>
             <div>
@@ -42,11 +53,35 @@ export default function DashboardContent({ products }: { products: Product[] }) 
                 >
                     Refresh
                 </button>
+                <div className="flex gap-3">
+                    <span>Name</span>
+                    <span>Price</span>
+                    <span>OrderBtn</span>
+                </div>
                 <ul className="w-full overflow-y-auto">
                     {products?.map((product: Product) => (
                         <li key={product.id} className="h-12 w-full bg-[#1f1f1f] flex justify-between">
                             <span>{product.name}</span>
                             <span>{product.price}</span>
+                            <span className="flex items-center">
+                                <button onClick={() => decrement(product.id)}>-</button>
+                                <span>{counts[product.id] ?? 0}</span>
+                                <button onClick={() => increment(product.id) }>+</button>
+                            </span>
+                            <span>
+                                <button
+                                    onClick={() => {
+                                        if (!counts[product.id]) {
+                                            console.log("plz select quantity to order");
+                                            return;
+                                        }
+                                        createOrder(product.id, product.price, counts[product.id]);
+                                    }}
+                                    className="cursor-pointer"
+                                >
+                                    Order
+                                </button>
+                            </span>
                         </li>
                     ))}
                 </ul>
